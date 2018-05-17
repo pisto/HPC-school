0d433db2bd0f69fd9c65c65856a172e845aa86d2
Lorenzo Pistone <blaffablaffa@gmail.com>
2018-05-17 16:56:04 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <stdexcept>
 #include <sstream>
-#include <random>
-#include <vector>
-#include <algorithm>
 #include <tuple>
 #include <chrono>
 #include <boost/core/demangle.hpp>
 #include <boost/program_options.hpp>
-#include "mpienv.hpp"
 #include "misc.hpp"
+#include "mpienv.hpp"
 
 using namespace std;
 namespace mpi = boost::mpi;
@@ -18,10 +14,10 @@ const mpi::environment mpienv [[gnu::init_priority(101)]] (mpi::threading::multi
 const mpi::communicator mpiworld;
 const int mpiworld_coord = mpiworld.rank();
 
-//archs: all x86_64, modern CPUs, my laptop, Galileo
+//archs: all x86_64, modern CPUs with YMM registers, my laptop, Galileo
 [[gnu::target_clones("default","avx","arch=ivybridge","arch=haswell")]]
 auto add_arrays(float* a, const float* __restrict__ b, size_t n, double sum_max){
-	double sum = 0.0;
+	double sum = 0;
 	for(size_t i_base = 0; i_base < n;) {
 		//speculatively try to calculate everything vectorized in bunches of bunch_len
 		constexpr const auto bunch_len = 4096;      //this is arbitrary
@@ -56,28 +52,22 @@ int main(int argc, char** argv) try {
 
 	uint32_t len;
 	double sum_max;
-	try {
+	{
 		using namespace boost::program_options;
 		options_description options("Options for mpibologna");
 		options.add_options()
-			("help", "show help")
-			("len", value(&len)->default_value(100000000), "vectors length")
-			("sum_max", value(&sum_max), "sum max (default half of len)");
+				("help", "show help")
+				("len", value(&len)->default_value(100000000), "vectors length")
+				("sum_max", value(&sum_max), "sum max (default half of len)");
 		variables_map vm;
 		store(parse_command_line(argc, argv, options), vm);
 		notify(vm);
-		if(vm.count("help")){
-			cerr<<options<<endl;
+		if (vm.count("help")) {
+			cerr << options << endl;
 			return 0;
 		}
-		if(!len) throw invalid_argument("len and bunch_size must be > 0");
-		if(!vm.count("sum_max")) sum_max = len / 2;
-	} catch(const invalid_argument& e){
-		cerr<<"Invalid argument found in command line: "<<e.what()<<endl;
-		return 1;
-	} catch(const boost::program_options::error& e){
-		cerr<<"Failed to parse command line: "<<e.what()<<endl;
-		return 1;
+		if (!len) throw invalid_argument("len and bunch_size must be > 0");
+		if (!vm.count("sum_max")) sum_max = len / 2;
 	}
 
 	vector<float> a(len), b(len);
